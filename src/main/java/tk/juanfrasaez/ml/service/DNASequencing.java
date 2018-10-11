a6ff2f48de9cb8e56860c55d7017380d9a7e4c4a
Francisco Saez <francisco.saez@cision.com>
2018-10-10 23:54:07 -0300

    Implement algorithm for Lvl 1 (horizontal and vertical matches) in DNASequencing service

--- src/main/java/tk/juanfrasaez/ml/service/DNASequencing.java
+++ src/main/java/tk/juanfrasaez/ml/service/DNASequencing.java
@@ -1,7 +1,47 @@
 package tk.juanfrasaez.ml.service;
 
 public class DNASequencing {
-    public boolean isMutant(String[] dna) {
-        return dna.length > 5 ? false : true;
+
+    /**
+     * This method analyzes a given nucleic acid sequence within a DNA molecule to determine if the DNA is mutant. If
+     * there is more than one sequence of four equal nucleobases (AAAA, CCCC, GGGG, TTTT), then the DNA is mutant.
+     *
+     * @param dna the string array representation of a DNA molecule
+     * @return {@code true} if the DNA is mutant, {@code false} otherwise.
+     * @throws IllegalArgumentException if dna is not a valid representation for an DNA molecule
+     */
+    public boolean isMutant(String[] dna) throws IllegalArgumentException {
+        if (dna == null || dna.length < 4)
+            throw new IllegalArgumentException();
+        int matchSequence = 0;
+        int horizontal = 0;
+        int[] vertical = new int[dna.length];
+        for (int row = 0; row < dna.length; row++) {
+            if (dna[row].length() != dna.length)
+                throw new IllegalArgumentException();
+            for (int column = 0; column < dna[row].length(); column++) {
+                if (!isValidNucleobaseRepresentation(dna[row].charAt(column)))
+                    throw new IllegalArgumentException();
+                horizontal += dna[row].charAt(column);
+                if (horizontal % dna[row].charAt(column) > 0) {
+                    horizontal = dna[row].charAt(column);
+                } else if (horizontal / 4 == dna[row].charAt(column)) {
+                    matchSequence++;
+                    horizontal = 0;
+                }
+                vertical[column] += dna[row].charAt(column);
+                if (vertical[column] % dna[row].charAt(column) > 0) {
+                    vertical[column] = dna[row].charAt(column);
+                } else if (vertical[column] / 4 == dna[row].charAt(column)) {
+                    matchSequence++;
+                    vertical[column] = 0;
+                }
+            }
+        }
+        return matchSequence > 1;
+    }
+
+    private boolean isValidNucleobaseRepresentation(char n) {
+        return ('A' == n || 'C' == n || 'G' == n || 'T' == n);
     }
 }
